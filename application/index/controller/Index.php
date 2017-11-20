547010c6895b287b919c8d228f0246e83bdeb6c4
c32 <amd5@qq.com>
2017-11-20 19:28:10 +0800

    2017-11-20 19:27:59

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -3,6 +3,7 @@ namespace app\index\controller;
 use think\Controller;
 use app\index\model\Article;
 use app\index\model\ArticleSort;
+use app\index\model\Link;
 
 class Index	extends Controller
 {
@@ -17,7 +18,6 @@ class Index	extends Controller
 		//分类列表
 		$articlesort = ArticleSort::where('status','=','1')
 		->select();
-		// dump($articlesort);
 		
 		//存档列表
 		//SELECT FROM_UNIXTIME(date,'%Y-%m') days,COUNT(*) COUNT FROM think_article GROUP BY days; 
@@ -26,7 +26,12 @@ class Index	extends Controller
 		->GROUP(days)
 		->select();
 
+		//友情链接
+		$links = Link::order('taxis','asc')
+		->select();
+
 		//输出
+		$this->assign('links', $links);
 		$this->assign('archives', $archives);
 		$this->assign('articlesort', $articlesort);
 		$this->assign('result', $result);
