8e9e5ecc5461ff056a6f9c9e7333605ab4e3a78e
c32 <amd5@qq.com>
2018-01-09 13:31:51 +0800

    修复git无法拉取的问题
    2018-1-9 13:31:49

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -225,6 +225,18 @@ class Index	extends BaseController
         pclose($handle);
     }
 
+    public function webhookspull()
+    {
+        //echo shell_exec("ifconfig");
+        echo "</br>";
+        error_reporting ( E_ALL );
+        $dir = '/home/wwwroot/web/domain/blog.19aq.com/web/';//该目录为git检出目录
+        $handle = popen('cd '.$dir.' && git pull','r');
+        $read = stream_get_contents($handle);
+        printf($read);
+        pclose($handle);
+    }
+
 
 	
 	
