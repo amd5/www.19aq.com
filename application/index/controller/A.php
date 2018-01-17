6a09310a8d8a665acfc9e83c175080ae8d7af38a
c32 <amd5@qq.com>
2018-01-17 19:56:16 +0800

    增加API测试POST功能
    2018-1-17 19:56:14

---
+++ application/index/controller/A.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace app\index\controller;
+use think\Controller;
+
+class A extends Controller
+{
+    public function index()
+    {
+    	$key	= "ghawvtiq";
+    	$data	= "B4F733BBA2CD88AD";  //加密后的用户密文
+
+		$check  =new a();
+		echo "密码为: " . $check->test($data,$key);
+    }
+
+    public function test($str,$key)
+    {
+    	$str = hex2bin($str);
+        $str = mcrypt_decrypt(MCRYPT_DES, $key, $str, MCRYPT_MODE_ECB);
+        $block = mcrypt_get_block_size('des', 'ecb');
+        $pad = ord($str[($len = strlen($str)) - 1]);
+        return substr($str, 0, strlen($str) - $pad);
+    }
+
+}
