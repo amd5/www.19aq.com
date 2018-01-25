9e4eee8624bbd0b776500a4f95271e062b4748e8
c32 <amd5@qq.com>
2018-01-25 14:14:50 +0800

    更新api查询ip地址记录物理地址
    2018-1-25 14:14:48

--- application/api/controller/Ip.php
+++ application/api/controller/Ip.php
@@ -15,11 +15,17 @@ class Ip extends Controller
         $dizhi = $this->request->ip();
         $xxip = $ipdizhi['ip'];
         $xxdz = $ipdizhi['country'].$ipdizhi['area'];
+        //输出查询IP的详细物理地址
         echo ($xxip.$xxdz);
+        //本地IP的详细物理地址
+        $local = $add->getlocation($dizhi);
+
         //获取本地IP、查询IP、查询时间
-        $data['localip'] = $dizhi;
-        $data['cheip']   = $ip;
-        $data['time']    = time();
+        $data['localip']    = $dizhi;
+        $data['localaddr']  = $local['country'].$local['area'];
+        $data['cheip']      = $ip;
+        $data['cheaddr']    = $ipdizhi['country'].$ipdizhi['area'];
+        $data['time']       = time();
         //插入数据库
         $result = Api_cheip::insert($data);
     }
