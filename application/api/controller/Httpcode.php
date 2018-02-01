e7b7c249d404e5741ad9ab0719fb3908c5b40632
c32 <amd5@qq.com>
2018-02-01 11:24:35 +0800

    2018-2-1 11:24:31

---
+++ application/api/controller/Httpcode.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\api\controller;
+
+use think\Controller;
+
+class Httpcode extends Controller
+{
+    public function index()
+    {
+        echo "string";
+    }
+
+}
--- application/index/controller/C.php
+++ application/index/controller/C.php
@@ -2,18 +2,15 @@
 
 namespace app\index\controller;
 use think\Controller;
-use app\index\model\Fenxiao;
+use app\index\model\Dataa;
 
 class C extends Controller
 {
     public function index()
     {
-    	//存档列表
-		// SELECT FROM_UNIXTIME(date,'%Y-%m') days,COUNT(*) COUNT FROM think_article GROUP BY days; 
-    	//SELECT distinct YEAR(FROM_UNIXTIME(date)) as nianfen FROM think_article ORDER BY nianfen DESC
-        echo "test";
-		$test = Fenxiao::select();
-        dump($test);
+		$test = Dataa::with('sort')->where('phone','15966982315')->find();
+        $this->assign('test', $test);
+        return $this->fetch();
 
     }
 
