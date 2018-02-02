39745b4f3c12cb214b403c8d18e4e4f5fa1d2d3f
c32 <amd5@qq.com>
2018-02-02 14:34:09 +0800

    2018-2-2 14:34:07

--- application/api/controller/Webscan.php
+++ application/api/controller/Webscan.php
@@ -10,64 +10,93 @@ class Webscan extends Controller
     public function index()
     {
         return 0;
+        //设定扫描参数->给予目标->加入队列->
     }
 
-    public function sao()
-    {
-    	$s = 10;
-    	//获取扫描目标
-    	$target = Api_webscan_target::select();
-    	//查询当前共多少条了
+    public function scan(){
+    	//查询当前一共扫了多少条了
     	$tiaoshu = Api_webscan_tmp::order('id desc')->limit(1)->select();
     	foreach ($tiaoshu as $key => $value) {
     		echo $value['id'];
     	}
 
-    	//给予扫描目标给curl
-        foreach ($target as $key => $value) {
-        	$ch 		= curl_init();  
-        	$url 		= $value['url'];
-			$timeout 	= 1;
-			// for ($i=0; $i < $s; $i++) { 
-				# code...
-				curl_setopt($ch, CURLOPT_URL, $url);  
-				curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);  
-				curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);  
-				curl_setopt($ch, CURLOPT_HEADER, 0);  
-				$info = curl_exec($ch);  
-				// dump($info);
+    	//获取扫描目标
+    	$target = Api_webscan_target::select();
 
-				if($info!=false)
-				{
-					// preg_match_all('/(src|href)="(.*?)"/',$info,$m);
-					preg_match('/(src|href)="(.*?)"/',$info,$m);
-					// dump($m);
-					if(strpos($m[2],'http')===false)
-					{
-						echo "链接内容没有网址前缀";
-						$t1  = $value['url'].$m[2];
-						$data = Api_webscan_tmp::where('url',$t1)->find();
-						if(!$data){
-							$tmp = Api_webscan_tmp::create(['url'=>$t1]);
-							echo "增加成功".$t1;
+    	$sm = new Webscan();
+    	$start = $sm->sao();
+
+    	foreach ($target as $key => $url) {
+    		# code...
+    		echo $url['url']."</br>";
+    	}
+
+    }
+
+    public function sao($url)
+    {
+		$ch 		= curl_init();  
+		$timeout 	= 1;
+		curl_setopt($ch, CURLOPT_URL, $url);  
+		curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);  
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);  
+		curl_setopt($ch, CURLOPT_HEADER, 0);  
+		$info = curl_exec($ch);  
+		// dump($info);
+
+		if($info!=false)
+		{
+			preg_match_all('/(src|href)="(.*?)"/',$info,$m);
+			// preg_match('/(src|href)="(.*?)"/',$info,$m);
+			// echo($m[2]);
+			foreach ($m[2] as $key => $body) {
+				// echo $value."</br>";
+				if (strpos($body,'base64') === false) {
+					# code...
+					if ($body !== '#') {
+						# code...
+						if ($body !== 'javascript:void()') {
+							# code...
+							if ($body !== $url) {
+								# code...
+								if(strpos($body,'http')===false)
+								{
+									// echo "链接内容没有网址前缀";
+									$t1  = $url.$body;
+									$data = Api_webscan_tmp::where('url',$t1)->find();
+									if(!$data){
+										$tmp = Api_webscan_tmp::create(['url'=>$t1]);
+										echo "增加成功".$t1."</br>";
+									}
+								}else{
+									//内容包含域名则直接新增
+									$tmp = Api_webscan_tmp::create(['url'=>$body]);
+									echo "增加成功".$body;
+								}
+							}
+							
 						}
-					}else{
-						//内容包含域名则直接新增
-						$tmp = Api_webscan_tmp::create(['url'=>$m[2]]);
+						
 					}
-				}else{
-					echo "没扫</br>";
+					
 				}
-				// echo "cccc";
-				dump($m[2]);
-				// dump($m);
-			// }
+
+				
+
+				
+				
+			}
+			
+		}else{
+			echo "没扫</br>";
+		}
+		// echo "cccc";
+		// dump($m[2]);
+		// dump($m);
 			
 
 
-			
-        }
 
         
 		
