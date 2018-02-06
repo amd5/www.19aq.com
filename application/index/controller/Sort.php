2b30171e8c4a762e743cda35a12529e4b99da109
c32 <amd5@qq.com>
2018-02-06 22:09:28 +0800

    修改查询数据到模型
    2018-2-6 22:09:26

--- application/index/controller/Sort.php
+++ application/index/controller/Sort.php
@@ -4,6 +4,7 @@ namespace app\index\controller;
 use think\Controller;
 use think\Request;    //请求IP地址等
 use app\index\model\Link;
+use app\index\model\Record;
 use app\index\model\Article;
 use app\index\model\ArticleTag;
 use app\index\model\ArticleSort;
@@ -12,80 +13,50 @@ class Sort extends Controller
 {
     public function index()
     {
+        $wenz = new Article;
         $request = Request::instance();
 
-        // dump($request->param('name'));
-        // dump($_GET["name"]);
         $sortid = ArticleSort::where('alias','=',$request->param('name'))->find();
-        // dump($sortid['sid']);
-        // die;
+        $sid = $sortid['sid'];
+
         //获取当前访问URL
         $url = "http://".$_SERVER['HTTP_HOST'];
-
+        
         if(!session('username'))
         {
             //文章列表  不是管理员显示没有密码的文章
-            $result = Article::with('sort')
-            ->order('id','desc')
-            ->where('password','=','')
-            ->where('sortid','=',$sortid['sid'])
-            ->limit(15)
-            ->paginate();
+            $result = $wenz->Articlelist($sid);
             $page = $result->render();   //获取分页显示
         }elseif(session('username') == "1")
         {
             //管理员ID不是1
-            $result = Article::with('sort')
-            ->order('id','desc')
-            ->where('password','=','')
-            ->where('sortid','=',$sortid['sid'])
-            ->limit(15)
-            ->paginate();
+            $result = $wenz->Articlelista($sid);
             $page = $result->render();
         }else
         {
             //文章列表  管理员显示全部文章
-            $result = Article::with('sort')
-            ->where('sortid','=',$sortid['sid'])
-            ->order('id','desc')
-            ->limit(15)
-            ->paginate();
+            $result = $wenz->Articleall($sid);
             $page = $result->render();
-            // print_r($result);
-
-            // $sort = $result->ArticleSort->sortname()
         }
 
         // $sort = 
 
         //文章标签
-        $tag = ArticleTag::select();
-        //dump($tag);
+        $taglist = new ArticleTag;
+        $tag     = $taglist->taglist();
 
         //分类列表
-        $articlesort = ArticleSort::withCount('sort')
-        ->where('status','=','1')
-        ->select();
+        $sort = new ArticleSort;
+        $articlesort = $sort->sortlist();
         
         //存档列表
-        // SELECT FROM_UNIXTIME(date,'%Y-%m') days,COUNT(*) COUNT FROM think_article GROUP BY days; 
-        //SELECT distinct YEAR(FROM_UNIXTIME(date)) as nianfen FROM think_article ORDER BY nianfen DESC
-        $nian = Article::order('days','desc')
-        ->field('FROM_UNIXTIME(date,"%Y") as days,COUNT(*) as COUNT')
-        ->GROUP('days')
-        ->select();
-
-        $yue = Article::order('days','desc')
-        ->field('FROM_UNIXTIME(date,"%Y年-%m月") as days,COUNT(*) as COUNT')
-        ->GROUP('days')
-        ->select();
-
-
-        
+        $record = new Record;
+        $nian = $record->nian();
+        $yue  = $record->yue();
 
         //友情链接
-        $links = Link::order('taxis','asc')
-        ->select();
+        $link = new link;
+        $links = $link->links();
 
         //输出
         $this->assign('url', $url);
