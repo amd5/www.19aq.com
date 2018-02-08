5912257eabd04e877052bd9d62657c295cbeba2d
c32 <amd5@qq.com>
2018-02-08 18:04:48 +0800

    增加初始化实例化模型
    2018-2-8 18:04:46

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -9,6 +9,7 @@ use app\index\model\Article;
 use app\index\model\ArticleSort;
 use app\index\model\ArticleTag;
 use app\index\model\Link;
+use app\index\model\Record;
 /*后台模块*/
 use app\admin\model\SystemConfig;
 /*其他第三方模块*/
@@ -18,9 +19,16 @@ use app\api\controller\Sendsms;
 use app\admin\controller\BaseController;
 // use app\index\controller\Check;
 
-// class Index extends BaseController
 class Index	extends Controller
 {
+    protected $wenz;
+
+    public function __construct()
+    {
+        $this->wenz = new Article;
+        parent::__construct();
+    }
+
     public function index()
     {
         //获取当前访问URL
@@ -41,64 +49,38 @@ class Index	extends Controller
     	if(!session('username'))
     	{
     		//文章列表  不是管理员显示没有密码的文章
-			$result = Article::with('sort')
-            ->order('id','desc')
-			->where('password','=','')
-			->limit(15)
-			->paginate();
+            $result = $this->wenz->Articles();
 			$page = $result->render();   //获取分页显示
     	}elseif(session('username') == "1")
     	{
     		//管理员ID不是1
-			$result = Article::with('sort')
-            ->order('id','desc')
-			->where('password','=','')
-			->limit(15)
-			->paginate();
+            $result = $this->wenz->Articles();
 			$page = $result->render();
     	}else
     	{
     		//文章列表  管理员显示全部文章
-			$result = Article::with('sort')
-            ->order('id','desc')
-			->limit(15)
-			->paginate();
+            $result = $this->wenz->Articleall();
 			$page = $result->render();
-            // print_r($result);
 
-            // $sort = $result->ArticleSort->sortname()
     	}
 
-        // $sort = 
 
     	//文章标签
-    	$tag = ArticleTag::select();
-    	//dump($tag);
+        $taglist = new ArticleTag;
+        $tag     = $taglist->taglist();
 
-		//分类列表
-		$articlesort = ArticleSort::withCount('sort')
-        ->where('status','=','1')
-		->select();
-		
-		//存档列表
-        // SELECT FROM_UNIXTIME(date,'%Y-%m') days,COUNT(*) COUNT FROM think_article GROUP BY days; 
-        //SELECT distinct YEAR(FROM_UNIXTIME(date)) as nianfen FROM think_article ORDER BY nianfen DESC
-        $nian = Article::order('days','desc')
-        ->field('FROM_UNIXTIME(date,"%Y") as days,COUNT(*) as COUNT')
-        ->GROUP('days')
-        ->select();
-
-        $yue = Article::order('days','desc')
-        ->field('FROM_UNIXTIME(date,"%Y年-%m月") as days,COUNT(*) as COUNT')
-        ->GROUP('days')
-        ->select();
-
-
+        //分类列表
+        $sort = new ArticleSort;
+        $articlesort = $sort->sortlist();
         
+        //存档列表
+        $record = new Record;
+        $nian = $record->nian();
+        $yue  = $record->yue();
 
-		//友情链接
-		$links = Link::order('taxis','asc')
-		->select();
+        //友情链接
+        $link = new link;
+        $links = $link->links();
 
 		//输出
         $this->assign('url', $url);
@@ -124,49 +106,6 @@ class Index	extends Controller
 		return $this->fetch();
         // return \think\Response::create(\think\Url::build('/admin'), 'redirect');
     }
-
-    // public function sort()
-    // {
-    //     echo "string";
-    //     $articlesort = ArticleSort::withCount('sort')
-    //     // ->alias('tiaoshu')
-    //     ->select();
-    //     $this->assign('articlesort', $articlesort);
-
-    //     return $this->fetch();
-    // }
-
-
-  //   public function tag()
-  //   {
-  //   	$result = Article::order('id','desc')
-		// 	->limit(15)
-		// 	->paginate();
-		// 	$page = $result->render();
-  //   	$tag = ArticleTag::select();
-  //   	// dump($tag);
-  //   	$this->assign('result', $result);
-		// $this->assign('page', $page);
-  //   	return $this->fetch();
-  //   }
-
-
-    // public function cheip($ip)
-    // {
-    //     $add=new IpLocation();
-    //     $ipdizhi = $add->getlocation($ip);
-    //     $dizhi = $this->request->ip();
-    //     $xxip = $ipdizhi['ip'];
-    //     $xxdz = $ipdizhi['country'].$ipdizhi['area'];
-    //     echo ("查询IP：".$xxip);
-    //     echo ($xxdz);
-
-    //     // $ipdizhi1 = $add->getlocation($dizhi);
-    //     // $xxip1 = $ipdizhi1['ip'];
-    //     // $xxdz1 = $ipdizhi1['country'].$ipdizhi1['area'];
-    //     // echo "</br>本机IP：".$xxip1;
-    //     // echo ($xxdz1);
-    // }
 
     public function test()
     {
