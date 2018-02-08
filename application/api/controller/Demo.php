d1d962a36e473bd23200298337acda5d6bbaa9f1
c32 <amd5@qq.com>
2018-02-08 18:03:13 +0800

    添加演示DEMO
    2018-2-8 18:03:10

---
+++ application/api/controller/Demo.php
@@ -0,0 +1,27 @@
+<?php
+namespace app\api\controller;
+
+use think\Controller;
+use app\api\model\User;
+/*其他第三方模块*/
+use app\extra\bitcoin\bitcoin;
+    const RPC_HOST = '127.0.0.1';
+    const RPC_PORT = '3233';
+    const RPC_USER = 'user';
+    const RPC_PASS = 'P2kAlMiG3Kb8FzP';
+class Demo extends Controller
+{
+    public function index()
+    {
+        echo "string";
+
+   //      $result = Article::with('sort')
+   //          ->order('id','desc')
+			// ->limit(15)
+			// ->paginate();
+			// $page = $result->render();
+    }
+
+
+
+}
