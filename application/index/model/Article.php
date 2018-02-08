ac3e164a7c7577786596932ab1759f6d24eb94b8
c32 <amd5@qq.com>
2018-02-08 18:04:31 +0800

    2018-2-8 18:04:27

--- application/index/model/Article.php
+++ application/index/model/Article.php
@@ -5,6 +5,7 @@
 # #Datetime:    2017-10-24 18:40:33
 # #Author:		c32
 # #Email:		amd5@qq.com
+# #感谢King east  QQ1207877378指导的模型关联
 # #########################################
 namespace app\index\model;
 
@@ -25,11 +26,11 @@ class Article extends Model
 	}
 
 	public function tag(){
-		return $this->hasMany('ArticleTag','gid','id');
+		return $this->hasMany('ArticleTag','gid');
 		//hasMany('关联模型名','外键名','主键名',['模型别名定义']);
 	}
 
-	//文章列表  不是管理员显示没有密码的文章
+	//分类页面--文章列表  不是管理员显示没有密码的文章
 	public function Articlelist($sid){
 		$result = Article::order('id','desc')
         ->where('password','=','')
@@ -40,19 +41,8 @@ class Article extends Model
 		return $result;
 	}
 
-	//管理员ID不是1 显示的文章列表
-	public function Articlelista($sid){
-		$result = Article::order('id','desc')
-        ->where('password','=','')
-        ->where('sortid','=',$sid)
-        ->limit(15)
-        ->paginate();
-
-		return $result;
-	}
-
-	//文章列表  管理员显示全部文章
-	public function Articleall($sid){
+	//分类页面--文章列表  管理员显示全部文章
+	public function Articlea($sid){
 		$result = Article::where('sortid','=',$sid)
         ->order('id','desc')
         ->limit(15)
@@ -60,12 +50,27 @@ class Article extends Model
         
 		return $result;
 	}
+	//显示全部文章（带密码的除外）
+	public function Articles(){
+        $result = Article::order('id','desc')
+		->where('password','=','')
+		->limit(15)
+		->paginate();
+
+		return $result;
+	}
+	//显示所有文章
+	public function Articleall(){
+        $result = Article::order('id','desc')
+		->limit(15)
+		->paginate();
+
+		return $result;
+	}
 
 	//标签文章列表
 	public function tagArticle($tags){
 		$result = Article::all($tags,'',false);
-		// dump($result);
-        
 		return $result;
 	}
 
