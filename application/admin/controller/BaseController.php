561237f0ccf1fdbf50e6f144d8d7bd03d63bbabc
c32 <amd5@qq.com>
2018-07-10 23:59:07 +0800

    优化登陆状态检查代码
    2018-7-10 23:58:59

--- application/admin/controller/BaseController.php
+++ application/admin/controller/BaseController.php
@@ -1,9 +1,7 @@
 <?php
-//感谢github作者zjw710
 namespace app\admin\controller;
 
 use think\Controller;
-// use think\Request;
 use think\Cache;
 use think\Session;
 
@@ -11,21 +9,16 @@ class BaseController extends Controller
 {
 	public function _initialize()
     {
-        session::init();
+        Session::init();
         $id    = session_id();
         $sid   = cache::get($id);
-        //判断用户是否登陆
-        if(!isset($sid)) {
-            //如果Session为空进入后台登录界面
-            if(!$_SERVER['PHP_SELF'] = "index.php"){
-                $this->redirect('./admin/login');
-            }
-        }else{
-            if((time() - $sid) > 7200)  //Session有效期 秒
-            {
-                Cache::rm($id);
-            }
 
+        if(isset($sid)) {
+            if((time() - $sid) > 7200){Cache::rm($id);}
+        }
+
+        if(!$_SERVER['PHP_SELF'] = "index.php"){
+            $this->redirect('./admin/login');
         }
     }
 
