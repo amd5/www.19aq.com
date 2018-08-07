836b8b47bb4ab03ebb8eb7345c812a1bc120cfdf
c32 <amd5@qq.com>
2018-08-07 09:44:38 +0800

    2018-8-7 09:44:33

--- application/database.php
+++ application/database.php
@@ -13,7 +13,7 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => 'localhost',
+    'hostname'        => 'gz-cdb-3e0pcfbd.sql.tencentcdb.com',
     // 数据库名
     'database'        => 'www_19aq_com',
     // 用户名
@@ -21,7 +21,7 @@ return [
     // 密码
     'password'        => 'aNWhGTKCGmT3v',
     // 端口
-    'hostport'        => '',
+    'hostport'        => '62464',
     // 连接dsn
     'dsn'             => '',
     // 数据库连接参数
